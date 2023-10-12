ba23b1fb2bda81c4b7be480a2d80f276a1d7f25b
mac <mac@MacBook-Air-de-mac.local>
2023-10-12 13:39:12 +0100

    Initial commit

---
+++ src/components/RegisterViews.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="registration">
+        <h3>Register here</h3>
+        <hr>
+        <div class="row" v-for="user in users">
+            <h4> {{ user.name }}</h4>
+            <button @click="registerUser(user)">Register</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['users'],
+    methods: {
+        registerUser(user) {
+            this.$emit('userRegistred', user);
+            user.registred = true;
+        }
+    },
+}
+</script>
